79fa52d77e3abe8ca4a24951897ed791b2275bc2
Spy Hunter <41215704+spyhuntergenral@users.noreply.github.com>
2024-10-09 10:06:46 +1300

    Update Utility.php

--- src/Utility.php
+++ src/Utility.php
@@ -118,7 +118,7 @@ function checkIfIpv6($ip){
 }
 
 function checkIpPort($ip){
-	if(strpos($ip, ".onion") || preg_match("/^\[{0,1}[0-9a-z:\.]{7,39}\]{0,1}:[0-9]{1,5}$/", $ip)) {
+	if(strpos($ip, ".onion") || strpos($ip, ".i2p") || preg_match("/^\[{0,1}[0-9a-z:\.]{7,39}\]{0,1}:[0-9]{1,5}$/", $ip)) {
 		return $ip;
 	}else{
 		return "Unknown";
